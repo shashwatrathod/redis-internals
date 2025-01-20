854106789e2a021f62bce52f3aff6fd9e77f2a3b
Shashwat Rathod <shashwatrathod22@gmail.com>
2025-01-20 14:16:21 -0500

    Add RESP decoding functions for basic datatypes and relevant unit tests

---
+++ core/resp.go
@@ -0,0 +1,170 @@
+package core
+
+import (
+	"errors"
+)
+
+const (
+	RespSimpleStringIdentifier = '+'
+	RespSimpleErrorIdentifier  = '-'
+	RespIntegerIdentifier      = ':'
+	RespBulkStringIdentifier   = '$'
+	RespArrayIdentifier        = '*'
+)
+
+// Decodes a RESP encoded simple string
+// and returns the decoded string, delta and error(if any)
+func decodeRespSimpleString(data []byte, startPos int) (string, int, error) {
+	if data[startPos] != RespSimpleStringIdentifier &&
+		data[startPos] != RespSimpleErrorIdentifier {
+		return "", startPos, errors.New("data is not a simple string")
+	}
+
+	startPos += 1
+	idx := findCarriageReturnIdx(data, startPos)
+
+	return string(data[startPos:idx]), idx + 2, nil
+}
+
+// Decodes a RESP encoded Bulk String.
+// Returns the decoded string, delta and errors (if any)
+func decodeRespBulkString(data []byte, startPos int) (string, int, error) {
+	if data[startPos] != RespBulkStringIdentifier {
+		return "", startPos, errors.New("data is not a bulk string")
+	}
+
+	startPos += 1
+	idx := findCarriageReturnIdx(data, startPos)
+
+	var length int = int(stoi(string(data[startPos:idx])))
+	idx += 2
+	endIdx := idx + length
+
+	return string(data[idx:endIdx]), endIdx + 2, nil
+}
+
+// Decodes a RESP encoded Simple Error.
+// Returns the decoded Error as a simple string, delta, and error (if any)
+func decodeRespSimpleError(data []byte, startPos int) (string, int, error) {
+	return decodeRespSimpleString(data, startPos)
+}
+
+// decodes a RESP encoded signed/unsigned integer.
+// returns the decoded int64, delta, and error (if any)
+func decodeRespInteger(data []byte, startPos int) (int64, int, error) {
+	if data[startPos] != RespIntegerIdentifier {
+		return -1, startPos, errors.New("data is not of int64 type")
+	}
+
+	startPos++
+
+	var sign int64 = 1
+
+	if data[startPos] == '+' || data[startPos] == '-' {
+		if data[startPos] == '-' {
+			sign = -1
+		}
+
+		startPos++
+	}
+
+	endIdx := findCarriageReturnIdx(data, startPos)
+
+	return sign * stoi(string(data[startPos:endIdx])), endIdx + 2, nil
+}
+
+// Decodes a RESP encoded array.
+// Returns the decoded array, delta and error (if any)
+func decodeRespArray(data []byte, startPos int) ([]interface{}, int, error) {
+
+	var res []interface{} = []interface{}{}
+
+	if data[startPos] != RespArrayIdentifier {
+		return res, startPos, errors.New("data is not of array type")
+	}
+
+	idx := findCarriageReturnIdx(data, startPos+1)
+
+	arrayLength := stoi(string(data[startPos+1 : idx]))
+	idx += 2
+
+	for iter := 0; iter < int(arrayLength); iter++ {
+		d, nextPos, err := DecodeOne(data, idx)
+		if err != nil {
+			return []interface{}{}, startPos, err
+		}
+
+		idx = nextPos
+		res = append(res, d)
+	}
+
+	return res, idx, nil
+}
+
+func DecodeOne(data []byte, startPos int) (interface{}, int, error) {
+	if len(data[startPos:]) == 0 {
+		return nil, 0, errors.New("no data")
+	}
+
+	switch data[startPos] {
+	case RespSimpleStringIdentifier:
+		return decodeRespSimpleString(data, startPos)
+	case RespSimpleErrorIdentifier:
+		return decodeRespSimpleError(data, startPos)
+	case RespBulkStringIdentifier:
+		return decodeRespBulkString(data, startPos)
+	case RespIntegerIdentifier:
+		return decodeRespInteger(data, startPos)
+	case RespArrayIdentifier:
+		return decodeRespArray(data, startPos)
+	default:
+		return nil, 0, errors.New("unknown datatype: " + string(data[startPos:]))
+	}
+}
+
+// Decode takes a byte slice as input and attempts to decode it into the given type interface{}.
+// It returns the decoded value and any error encountered during the decoding process.
+//
+// Parameters:
+// - data: A byte slice containing the data to be decoded.
+//
+// Returns:
+// - An interface{} representing the decoded value.
+// - An error if the decoding process fails or if the input data is empty.
+func Decode(data []byte) (interface{}, error) {
+	if len(data) == 0 {
+		return nil, errors.New("no data")
+	}
+
+	value, _, err := DecodeOne(data, 0)
+	return value, err
+}
+
+// converts the given string into an int64.
+// assumes that the numString is a valid integer.
+func stoi(numString string) int64 {
+	var num int64 = 0
+	for i := 0; i < len(numString); i++ {
+		num = num*10 + int64(numString[i]-'0')
+	}
+
+	return num
+}
+
+// Finds and returns the index of the '\r' (carriage return)
+// character in the byte array. Returns -1 if \r not found.
+func findCarriageReturnIdx(data []byte, startPos int) int {
+	crIdx := -1
+
+	idx := startPos
+	for {
+		if data[idx] == '\r' {
+			crIdx = idx
+			break
+		} else {
+			idx++
+		}
+	}
+
+	return crIdx
+}
---
+++ core/resp_test.go
@@ -0,0 +1,117 @@
+package core
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestResp(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "RESP Suite")
+}
+
+type DecodeTestCase struct {
+	Input          string
+	ExpectedOutput interface{}
+	ExpectError    bool
+}
+
+func getDecodeTestCase(input string, expectedOutput interface{}, expectError bool) DecodeTestCase {
+	ret := DecodeTestCase{
+		Input:          input,
+		ExpectedOutput: expectedOutput,
+		ExpectError:    expectError,
+	}
+	return ret
+}
+
+func runTestAgainstCase(testCase DecodeTestCase) {
+	result, err := Decode([]byte(testCase.Input))
+
+	validateResult(testCase, result, err)
+}
+
+func validateResult(testCase DecodeTestCase, result interface{}, err error) {
+	if !testCase.ExpectError {
+		Expect(err).ToNot(HaveOccurred())
+	} else {
+		Expect(err).To(HaveOccurred())
+	}
+
+	Expect(result).To(Equal(testCase.ExpectedOutput))
+}
+
+var _ = Describe("Decode", func() {
+	Context("Simple String", func() {
+		It("Should decode a simple string", func() {
+			test := getDecodeTestCase("+OK\r\n", "OK", false)
+			runTestAgainstCase(test)
+		})
+	})
+
+	Context("Bulk String", func() {
+		It("Should decode a bulk string", func() {
+			test := getDecodeTestCase("$5\r\nHello\r\n", "Hello", false)
+			runTestAgainstCase(test)
+		})
+	})
+
+	Context("Integer64", func() {
+		It("Should decode an integer without any signs", func() {
+			test := getDecodeTestCase(":2345\r\n", int64(2345), false)
+			runTestAgainstCase(test)
+		})
+
+		It("Should decode an integer with + sign", func() {
+			test := getDecodeTestCase(":+2345\r\n", int64(2345), false)
+			runTestAgainstCase(test)
+		})
+
+		It("Should decode an integer with - sign", func() {
+			test := getDecodeTestCase(":-2345\r\n", int64(-2345), false)
+			runTestAgainstCase(test)
+		})
+	})
+
+	Context("Simple Error", func() {
+		It("Should decode a simple error", func() {
+			test := getDecodeTestCase("-Error message\r\n", "Error message", false)
+			runTestAgainstCase(test)
+		})
+	})
+
+	Context("Arrays", func() {
+		It("Should decode an empty array", func() {
+			test := getDecodeTestCase("*0\r\n", []interface{}{}, false)
+			runTestAgainstCase(test)
+		})
+
+		It("Should decode an array with simple strings", func() {
+			test := getDecodeTestCase("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n", []interface{}{"hello", "world"}, false)
+			runTestAgainstCase(test)
+		})
+
+		It("Should decode an array with int64", func() {
+			test := getDecodeTestCase("*3\r\n:1\r\n:2\r\n:3\r\n", []interface{}{int64(1), int64(2), int64(3)}, false)
+			runTestAgainstCase(test)
+		})
+
+		It("Should decode an array with different datatypes", func() {
+			test := getDecodeTestCase("*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n",
+				[]interface{}{int64(1), int64(2), int64(3), int64(4), "hello"},
+				false)
+			runTestAgainstCase(test)
+		})
+
+		It("Should decode nested arrays", func() {
+			test := getDecodeTestCase("*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*3\r\n+Hello\r\n-World\r\n:-19\r\n",
+				[]interface{}{
+					[]interface{}{int64(1), int64(2), int64(3)},
+					[]interface{}{"Hello", "World", int64(-19)}},
+				false)
+			runTestAgainstCase(test)
+		})
+	})
+})

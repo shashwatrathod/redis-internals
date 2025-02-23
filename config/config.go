187bf09991b712b9bddeb7820f05ade3d0092a39
Shashwat Rathod <shashwatrathod22@gmail.com>
2025-02-23 14:45:01 -0500

    feat: add config param "eviction ratio" to control the amount of keys to be evicted

--- config/config.go
+++ config/config.go
@@ -3,12 +3,18 @@ package config
 var Host string = "localhost"
 var Port int = 7379
 
+// logs the raw request body if set to true.
 var LogRequest bool = false
 
 // storage config
-var MaxKeys int = 10
+
+// maximum number of keys that can store in the store before eviction kicks in
+var MaxKeys int = 100
 
 // eviction policy config parameters
+
+// ratio of keys to be evicted everytime the an eviction is performed.
+var EvictionRatio float32 = 0.20
 
 // number of keys to sample while selecting the best candidate for removal.
 var LRUEvictionSampleSize int = 5
